a86fbb9c04ab338338904edab7bf0806494590e1
vignesh.sivaprakasam <vignesh.sivaprakasam@zohocorp.com>
2020-04-05 14:28:03 +0530

    Removed Mysql Dependencies and JAVA files

--- src/main/java/com/vicky/sprest/card/CardOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vicky.sprest.card;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CardOrderRepository extends CrudRepository<CardOrder, Long>{
-	public CardOrder findByStackId(long stackID);
-}
--- src/main/java/com/vicky/sprest/stack/StackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vicky.sprest.stack;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface StackRepository extends CrudRepository<Stack, Long>{
-	public List<Stack> findByBoardId(long boardID);
-}
